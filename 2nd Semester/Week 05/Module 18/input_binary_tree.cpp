71a65209a11440d36b983f88951b33d91b8e1f85
md-zeon <zeonrahaman5870@gmail.com>
2024-09-02 22:13:24 +0600

    Module 17 Complete

---
+++ 2nd Semester/Week 05/Module 18/input_binary_tree.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int data) {
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+void input_tree() {
+    int val;
+    cin >> val;
+}
+
+int main() {
+
+    
+
+    return 0;
+}
